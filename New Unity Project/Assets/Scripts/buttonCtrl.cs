6061b5d9f45bda2412d7b0c93d341a4456bd06d0
Maksymilian Tym <tymszczur@gmail.com>
2019-04-27 15:02:35 +0200

    Imported Easy Save 3

---
+++ New Unity Project/Assets/Scripts/buttonCtrl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class buttonCtrl : MonoBehaviour
+{
+ 
+	void OnQuitButton(){
+
+		Application.Quit();
+
+	}
+
+	void OnStartButton(){
+
+		SceneManager.LoadScene("LVL1");
+
+	}
+
+}
