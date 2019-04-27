db784c35c24cff96aef982e0b59b459b1542e51e
Maksymilian Tym <tymszczur@gmail.com>
2019-04-27 17:35:09 +0200

    """""FIXED""""" the colliders

--- New Unity Project/Assets/Scripts/lifeManager.cs
+++ New Unity Project/Assets/Scripts/lifeManager.cs
@@ -9,11 +9,13 @@ public class lifeManager : MonoBehaviour
 	public Slider healthSlider;
 	public float health;
 	public Text healthText;
+	public List<Sprite> character;
+	public GameObject player;
 
     // Start is called before the first frame update
     void Start()
     {
-
+    	player = GameObject.Find("Player");
     }
 
     // Update is called once per frame
@@ -26,6 +28,30 @@ public class lifeManager : MonoBehaviour
         if(health <= 0f)
         {
         	GameOver();
+        }
+        if(health > 50f)
+        {
+        	player.GetComponent<SpriteRenderer>().sprite = character[0];
+        }
+        if(health > 40f && health < 50)
+        {
+        	player.GetComponent<SpriteRenderer>().sprite = character[1];
+        }
+        if(health > 30f && health < 40)
+        {
+        	player.GetComponent<SpriteRenderer>().sprite = character[2];
+        }
+        if(health > 20f && health < 30)
+        {
+        	player.GetComponent<SpriteRenderer>().sprite = character[3];
+        }
+        if(health > 10f && health < 20)
+        {
+        	player.GetComponent<SpriteRenderer>().sprite = character[4];
+        }
+        if(health < 10f)
+        {
+        	player.GetComponent<SpriteRenderer>().sprite = character[5];
         }
     }
 
