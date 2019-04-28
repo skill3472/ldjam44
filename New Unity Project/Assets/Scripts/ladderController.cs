98bd57984c52b77f505212d24d96567805e063bd
Maksymilian Tym <tymszczur@gmail.com>
2019-04-28 20:37:10 +0200

    Created a ladder manager prefab

---
+++ New Unity Project/Assets/Scripts/ladderController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ladderController : MonoBehaviour
+{
+	
+	public GameObject endpoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        endpoint = gameObject.transform.GetChild(0).gameObject;
+    }
+
+
+
+    void OnTriggerEnter2D(Collider2D col){
+
+    	if(col.gameObject.tag == "Player")
+    	{
+    		Debug.Log("Touching Player!");
+    		col.gameObject.transform.position = endpoint.transform.position;
+    	}
+
+    }
+}
